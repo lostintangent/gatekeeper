c1efa460fb1e59e685f54b35edb3802c6ca3b712
olegoid <oldemche@microsoft.com>
2020-06-09 21:31:05 -0400

    Readjust gatekeeper to a newer policy API

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,7 @@
 import * as vscode from "vscode";
 import * as vsls from "vsls";
 
-const EXTENSION_NAME = "liveshare";
 export async function activate(context: vscode.ExtensionContext) {
-  // Ensure the neccessary settings are
-  // re-enforced for the end-user.
-  await enforceSettings();
 
   // This extension takes a hard depedency on
   // Live Share, so it will always be available.
@@ -14,59 +10,29 @@ export async function activate(context: vscode.ExtensionContext) {
   // Wait for any guests to attempt to join
   // a collaboration session, in order to
   // determine if they're allowed in or not.
-  api.onDidChangePeers((e) => {
-    e.added.forEach((peer) => {
-      // If the current user doesn't have an e-mail address, then
-      // there's no point in us trying to match it against guests.
-      const selfDomain = api.session.user?.emailAddress?.split("@")[1];
-      if (!selfDomain) {
-        return;
-      }
 
-      // If the incoming user doesn't have an e-mail address,
-      // then immediately remove them, since anonymous/unknown
-      // guests aren't allowed, regardless of the host's settings.
-      const emailDomain = peer.user?.emailAddress?.split("@")[1];
-      if (!emailDomain) {
-        return removeUser(peer.peerNumber);
-      }
-
-      // If the incoming user is from the same domain
-      // as the host, then they're immediately allowed.
-      if (selfDomain.localeCompare(emailDomain) === 0) {
-        return;
-      }
-
-      const allowedDomains: string[] = vscode.workspace
-        .getConfiguration(EXTENSION_NAME)
-        .get("allowedDomains", []);
-
-      // If the incoming user is from a different domain,
-      // which hasn't been whitelisted, then remove them.
-      if (!allowedDomains.includes(emailDomain)) {
-        removeUser(peer.peerNumber);
-      }
-    });
-  });
+  let policyProvider = new TestPolicyProvider();
+  api.registerPolicyProvider("Test Policy Provider", policyProvider);
 }
 
-function removeUser(peerNumber: number) {
-  vscode.commands.executeCommand(`${EXTENSION_NAME}.removeParticipant`, {
-    sessionId: peerNumber,
-  });
+class TestPolicyProvider implements vsls.PolicyProvider {
+
+  policies: vsls.Policy[] = [
+    new GenericPolicy(vsls.SettingKey.AllowGuestDebugControl, false)
+  ];
 }
 
-const SETTINGS = [
-  "allowGuestDebugControl",
-  "allowGuestTaskControl",
-  "autoShareServers"
-];
+class GenericPolicy implements vsls.Policy {
 
-async function enforceSettings() {
-  const config = vscode.workspace.getConfiguration(EXTENSION_NAME);
-  return Promise.all(
-    SETTINGS.map((setting) =>
-      config.update(setting, false, vscode.ConfigurationTarget.Global)
-    )
-  );
+  constructor(settingKey: vsls.SettingKey, value: any, isUserLevel: boolean = true) {
+    this.isUserLevel = isUserLevel;
+    this.value = value;
+    this.settingKey = settingKey;
+  }
+
+  isUserLevel: boolean;
+  
+  value: any;
+  
+  settingKey: vsls.SettingKey;
 }

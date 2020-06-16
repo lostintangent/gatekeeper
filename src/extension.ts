adf85d404c794c959343a41899480b2272a85e67
olegoid <oldemche@microsoft.com>
2020-06-16 19:03:55 -0400

    Update the extension with the most recent API changes

--- src/extension.ts
+++ src/extension.ts
@@ -5,35 +5,36 @@ export async function activate(context: vscode.ExtensionContext) {
   const api = (await vsls.getApi())!;
 
   let policyProvider = new TestPolicyProvider();
-  let handler = api.registerPolicyProvider("Strict Policy Provider", policyProvider);
+  api.registerPolicyProvider("Strict Policy Provider", policyProvider);
 
-  await vscode.window.showErrorMessage("Hi! This message comes from gatekeeper extension. Close the notification to turn my policies off.");
-  handler?.dispose();
+  // await vscode.window.showErrorMessage("Hi! This message comes from gatekeeper extension. Close the notification to turn my policies off.");
+  // handler?.dispose();
 }
 
 class TestPolicyProvider implements vsls.PolicyProvider {
 
   policies: vsls.Policy[] = [
-    new GenericPolicy(vsls.SettingKey.AnonymousGuestApproval, "accept"),
-    new GenericPolicy(vsls.SettingKey.AllowGuestDebugControl, false),
-    new GenericPolicy(vsls.SettingKey.AllowedDomains, [
+    new GenericPolicy(vsls.PolicyTitle.AnonymousGuestApproval, "accept"),
+    new GenericPolicy(vsls.PolicyTitle.AllowGuestDebugControl, false),
+    new GenericPolicy(vsls.PolicyTitle.AllowedDomains, [
       "microsoft.com",
       "github.com"
     ]),
+    new GenericPolicy(vsls.PolicyTitle.ReadOnlyTerminal, true)
   ];
 }
 
 class GenericPolicy implements vsls.Policy {
 
-  constructor(settingKey: vsls.SettingKey, value: any, isUserLevel: boolean = false) {
+  constructor(policyTitle: vsls.PolicyTitle, value: any, isUserLevel: boolean = false) {
     this.isUserLevel = isUserLevel;
     this.value = value;
-    this.settingKey = settingKey;
+    this.policyTitle = policyTitle;
   }
 
   isUserLevel: boolean;
   
   value: any;
   
-  settingKey: vsls.SettingKey;
+  policyTitle: vsls.PolicyTitle;
 }

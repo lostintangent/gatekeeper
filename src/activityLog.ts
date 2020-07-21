a99a1a522fde99438962124a63d2705433b8cea9
Dayton Ellwanger <daellwan@microsoft.com>
2020-07-21 17:15:25 -0400

    JSON lines support (#8)

--- src/activityLog.ts
+++ src/activityLog.ts
@@ -1,22 +1,18 @@
-import * as yaml from "js-yaml";
 import * as os from "os";
 import * as path from "path";
 import OutputFile from "./outputFile";
 
 export default class ActivityLog {
   private outputFile = new OutputFile(
-    "ActivityLog.yaml",
+    "ActivityLog.jsonl",
     path.join(os.tmpdir(), "VSLiveShareActivityLogs")
   );
 
   public async openAsync(): Promise<void> {
     await this.outputFile.openAsync();
-    this.outputFile.append(`activities:${os.EOL}`);
   }
 
   public log(entry: any): void {
-    this.outputFile.append(
-      `${yaml.safeDump([entry], { skipInvalid: true })}${os.EOL}`
-    );
+    this.outputFile.append(`${JSON.stringify(entry)}${'\n'}`);
   }
 }
